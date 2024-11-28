49e7010494336ce8a0eb76f08acd932a38bd50f7
Yncy0 <paalayancy@gmail.com>
2024-11-28 21:06:50 +0800

    checkpoint

--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,16 +1,21 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Schedule() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff"
-      }}
-    >
-      <Text>Schedule</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          gap: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Text style={{width: '100%', paddingHorizontal: 20}}>History</Text>
+        <Text style={{width: '100%', paddingHorizontal: 20}}>My Schedule</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
