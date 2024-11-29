6b8d5ff03afb1ea5986875b72c7bed471c641db6
Yncy0 <paalayancy@gmail.com>
2024-11-29 22:13:57 +0800

    add: date card layout

--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,9 +1,27 @@
 import HistoryStatus from "@/components/HistoryStatus";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import moment from "moment";
 import { DATA } from "@/data/DATA";
+import DateCard from "@/components/DateCard";
+
+
+const generateDatesForCurrentMonth = () => { 
+  const startOfMonth = moment().startOf('month'); 
+  const endOfMonth = moment().endOf('month'); 
+  const dates = []; 
+  
+  let currentDate = startOfMonth; 
+  while (currentDate <= endOfMonth) { 
+      dates.push(currentDate.clone()); 
+      currentDate.add(1, 'day'); 
+    } 
+  return dates; 
+};
 
 export default function Schedule() {
+  const dates = generateDatesForCurrentMonth();
+
   return (
     <SafeAreaProvider>
       <SafeAreaView
@@ -15,13 +33,21 @@ export default function Schedule() {
           backgroundColor: "#fff",
         }}
       >
-        <Text style={{width: '100%', paddingHorizontal: 20}}>History</Text>
+        <Text style={{width: '100%', paddingHorizontal: 20, paddingTop: 20}}>History</Text>
         <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems: 'center', width: '100%', paddingHorizontal: 20}}>
           <HistoryStatus status="Incoming" color="#FFDB5E"/>
           <HistoryStatus status="Completed" color="#81A4D8"/>
           <HistoryStatus status="Cancelled" color="#FF5C5C"/>
         </View>
         <Text style={{width: '100%', paddingHorizontal: 20}}>My Schedule</Text>
+        <FlatList 
+          data={dates} 
+          keyExtractor={(item, index) => index.toString()} 
+          renderItem={({ item }) => <DateCard date={item} />}  
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{gap: 10, paddingHorizontal: 20}}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
