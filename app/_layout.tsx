81b008fff434e6a70d2cef248cdb124967c20c5f
Yncy0 <paalayancy@gmail.com>
2025-01-07 14:29:05 +0800

    added tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
+import React from "react";
 import AuthProvider from "@/context/AuthProvider";
 import { Stack } from "expo-router";
 import { setStatusBarStyle } from "expo-status-bar";
-import React from "react";
+import { TamaguiProvider } from "@tamagui/core";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  if (!loaded) return null;
+
   React.useEffect(() => {
     setTimeout(() => {
       setStatusBarStyle("dark");
@@ -12,10 +21,12 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
-      </Stack>
+      <TamaguiProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+      </TamaguiProvider>
     </AuthProvider>
   );
 }
