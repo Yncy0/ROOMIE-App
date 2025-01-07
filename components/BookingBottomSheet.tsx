2378f0b2582f308327e04740ba19c408f78c9d04
Yncy0 <paalayancy@gmail.com>
2025-01-07 09:34:16 +0800

    change layout in time -> BookingBottomSheet.tsx

--- components/BookingBottomSheet.tsx
+++ components/BookingBottomSheet.tsx
@@ -2,34 +2,43 @@ import React, { PropsWithChildren } from "react";
 import { Modal, Pressable, View } from "react-native";
 import IconTextInput from "./IconTextInput";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import BottomSheet, { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 import OvalButton from "./OvalButton";
 
-
-export default function BookingBottomSheet() {  
-    return (
-        <BottomSheetView
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                backgroundColor:'white',
-                width: '100%',
-                height: 425,
-                paddingHorizontal: 20,
-                paddingTop: 20,
-                paddingBottom: 50
-            }}
+export default function BookingBottomSheet() {
+  return (
+    <BottomSheetView
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "white",
+        width: "100%",
+        height: 425,
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        paddingBottom: 50,
+      }}
+    >
+      <View style={{ width: "100%", gap: 20 }}>
+        <IconTextInput icon={"book"} placeholder="Subject Name" />
+        <IconTextInput icon={"people-alt"} placeholder="Course & Section" />
+        <IconTextInput icon={"calendar-today"} placeholder="Date" />
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
         >
-            <View
-                style={{width: '100%', gap: 20}}
-            >
-                <IconTextInput icon={'book'} placeholder="Subject Name"/>
-                <IconTextInput icon={'people-alt'} placeholder="Course & Section"/>
-                <IconTextInput icon={'calendar-today'} placeholder="Date"/>
-                <IconTextInput icon={'schedule'} placeholder="Time"/>  
-            </View>
-            <OvalButton text="Reserve" color="black" height={50} width={250}/>
-        </BottomSheetView>
-    )
+          <IconTextInput icon={"schedule"} placeholder="Time-in" />
+          <IconTextInput icon={"schedule"} placeholder="Time-out" />
+        </View>
+      </View>
+      <OvalButton text="Reserve" color="black" height={50} width={250} />
+    </BottomSheetView>
+  );
 }
--- components/IconTextInput.tsx
+++ components/IconTextInput.tsx
@@ -2,34 +2,34 @@ import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { TextInput, View } from "react-native";
 
-
 type Props = {
-    icon: any,
-    placeholder: string,
-}
+  icon: any;
+  placeholder: string;
+};
 
 export default function IconTextInput({ icon, placeholder }: Props) {
-    const [text, setText] = React.useState<string>("");
+  const [text, setText] = React.useState<string>("");
 
-    return (
-        <View
-            style={{
-                width: '100%',
-                flexDirection: 'row',
-                alignItems: 'center',
-                borderWidth: 2,
-                borderColor: 'black',
-                borderRadius: 10,
-                gap: 20,
-                padding: 10,
-            }}
-        >
-            <MaterialIcons name={icon} size={24}/>
-            <TextInput 
-                placeholder={placeholder}
-                value={text}
-                onChangeText={setText}
-            />
-        </View>
-    )
+  return (
+    <View
+      style={{
+        minWidth: "48%",
+        maxWidth: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 2,
+        borderColor: "black",
+        borderRadius: 10,
+        gap: 20,
+        padding: 10,
+      }}
+    >
+      <MaterialIcons name={icon} size={24} />
+      <TextInput
+        placeholder={placeholder}
+        value={text}
+        onChangeText={setText}
+      />
+    </View>
+  );
 }
