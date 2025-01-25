347a0795a29f38ef7cc2fe403fccee2fe9ac3938
Yncy0 <paalayancy@gmail.com>
2025-01-25 11:36:44 +0800

    finally fixed migration conflicts

---
+++ supabase/migrations/20250125033542_remote_schema.sql
@@ -0,0 +1,701 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+
+
+
+
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION "public"."handle_new_user"() RETURNS "trigger"
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    SET "search_path" TO ''
+    AS $$
+begin
+  insert into public.profiles (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$$;
+
+
+ALTER FUNCTION "public"."handle_new_user"() OWNER TO "postgres";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+
+CREATE TABLE IF NOT EXISTS "public"."booked_rooms" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "time_in" timestamp with time zone,
+    "time_out" timestamp with time zone,
+    "course_and_section" "text",
+    "subject_code" "text",
+    "profile_id" "uuid",
+    "room_id" "uuid"
+);
+
+
+ALTER TABLE "public"."booked_rooms" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."building" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "building_name" "text"
+);
+
+
+ALTER TABLE "public"."building" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."course" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "course_name" "text"
+);
+
+
+ALTER TABLE "public"."course" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."profiles" (
+    "id" "uuid" NOT NULL,
+    "updated_at" timestamp with time zone,
+    "username" "text",
+    "full_name" "text",
+    "avatar_url" "text",
+    "website" "text",
+    CONSTRAINT "username_length" CHECK (("char_length"("username") >= 3))
+);
+
+
+ALTER TABLE "public"."profiles" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."rooms" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "room_name" "text",
+    "room_image" "text",
+    "room_type" "text",
+    "room_capacity" bigint,
+    "building_id" "uuid"
+);
+
+
+ALTER TABLE "public"."rooms" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."schedule" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "days" "text",
+    "subject_id" "uuid",
+    "course_id" "uuid",
+    "profile_id" "uuid",
+    "room_id" "uuid",
+    "status" "text",
+    "time_in" time with time zone,
+    "time_out" time with time zone,
+    "timef_in" time without time zone,
+    "timef_out" time without time zone
+);
+
+
+ALTER TABLE "public"."schedule" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."subject" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "subject_name" "text",
+    "subject_code" "text",
+    "unit_lecture" bigint DEFAULT '0'::bigint,
+    "unit_lab" bigint DEFAULT '0'::bigint
+);
+
+
+ALTER TABLE "public"."subject" OWNER TO "postgres";
+
+
+ALTER TABLE ONLY "public"."booked_rooms"
+    ADD CONSTRAINT "booked_rooms_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."building"
+    ADD CONSTRAINT "building_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."course"
+    ADD CONSTRAINT "course_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_username_key" UNIQUE ("username");
+
+
+
+ALTER TABLE ONLY "public"."rooms"
+    ADD CONSTRAINT "rooms_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."schedule"
+    ADD CONSTRAINT "schedule_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."subject"
+    ADD CONSTRAINT "subject_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."booked_rooms"
+    ADD CONSTRAINT "booked_rooms_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."booked_rooms"
+    ADD CONSTRAINT "booked_rooms_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "public"."rooms"("id");
+
+
+
+ALTER TABLE ONLY "public"."booked_rooms"
+    ADD CONSTRAINT "profile_id" FOREIGN KEY ("id") REFERENCES "public"."profiles"("id");
+
+
+
+ALTER TABLE ONLY "public"."profiles"
+    ADD CONSTRAINT "profiles_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."booked_rooms"
+    ADD CONSTRAINT "room_id" FOREIGN KEY ("id") REFERENCES "public"."rooms"("id");
+
+
+
+ALTER TABLE ONLY "public"."rooms"
+    ADD CONSTRAINT "rooms_building_id_fkey" FOREIGN KEY ("building_id") REFERENCES "public"."building"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."schedule"
+    ADD CONSTRAINT "schedule_course_id_fkey" FOREIGN KEY ("course_id") REFERENCES "public"."course"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."schedule"
+    ADD CONSTRAINT "schedule_profile_id_fkey" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."schedule"
+    ADD CONSTRAINT "schedule_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "public"."rooms"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."schedule"
+    ADD CONSTRAINT "schedule_subject_id_fkey" FOREIGN KEY ("subject_id") REFERENCES "public"."subject"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+CREATE POLICY "ALL" ON "public"."building" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "ALL" ON "public"."course" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "ALL" ON "public"."schedule" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "ALL" ON "public"."subject" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "Enable delete for users based on user_id" ON "public"."booked_rooms" FOR DELETE USING (true);
+
+
+
+CREATE POLICY "Enable insert for anon users only" ON "public"."rooms" FOR INSERT TO "anon" WITH CHECK (true);
+
+
+
+CREATE POLICY "Enable insert for authenticated users only" ON "public"."booked_rooms" FOR INSERT TO "authenticated" WITH CHECK (true);
+
+
+
+CREATE POLICY "Enable insert for authenticated users only" ON "public"."rooms" FOR INSERT TO "authenticated" WITH CHECK (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."booked_rooms" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."building" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."course" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."rooms" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."schedule" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."subject" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable update for users based on email" ON "public"."booked_rooms" FOR UPDATE USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "Public profiles are viewable by everyone." ON "public"."profiles" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Update Anon" ON "public"."rooms" FOR UPDATE TO "anon" USING (true) WITH CHECK (true);
+
+
+
+CREATE POLICY "Users can insert their own profile." ON "public"."profiles" FOR INSERT WITH CHECK ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+CREATE POLICY "Users can update own profile." ON "public"."profiles" FOR UPDATE USING ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+ALTER TABLE "public"."booked_rooms" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."building" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."course" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."profiles" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."rooms" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."schedule" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."subject" ENABLE ROW LEVEL SECURITY;
+
+
+CREATE PUBLICATION "realtime_messages_publication_v2_34_1" WITH (publish = 'insert, update, delete, truncate');
+
+
+ALTER PUBLICATION "realtime_messages_publication_v2_34_1" OWNER TO "supabase_admin";
+
+
+
+
+ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
+
+
+CREATE PUBLICATION "supabase_realtime_messages_publication_v2_34_2" WITH (publish = 'insert, update, delete, truncate');
+
+
+ALTER PUBLICATION "supabase_realtime_messages_publication_v2_34_2" OWNER TO "supabase_admin";
+
+
+CREATE PUBLICATION "supabase_realtime_messages_publication_v2_34_6" WITH (publish = 'insert, update, delete, truncate');
+
+
+ALTER PUBLICATION "supabase_realtime_messages_publication_v2_34_6" OWNER TO "supabase_admin";
+
+
+ALTER PUBLICATION "supabase_realtime" ADD TABLE ONLY "public"."booked_rooms";
+
+
+
+ALTER PUBLICATION "supabase_realtime" ADD TABLE ONLY "public"."schedule";
+
+
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "anon";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "authenticated";
+GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GRANT ALL ON TABLE "public"."booked_rooms" TO "anon";
+GRANT ALL ON TABLE "public"."booked_rooms" TO "authenticated";
+GRANT ALL ON TABLE "public"."booked_rooms" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."building" TO "anon";
+GRANT ALL ON TABLE "public"."building" TO "authenticated";
+GRANT ALL ON TABLE "public"."building" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."course" TO "anon";
+GRANT ALL ON TABLE "public"."course" TO "authenticated";
+GRANT ALL ON TABLE "public"."course" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."profiles" TO "anon";
+GRANT ALL ON TABLE "public"."profiles" TO "authenticated";
+GRANT ALL ON TABLE "public"."profiles" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."rooms" TO "anon";
+GRANT ALL ON TABLE "public"."rooms" TO "authenticated";
+GRANT ALL ON TABLE "public"."rooms" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."schedule" TO "anon";
+GRANT ALL ON TABLE "public"."schedule" TO "authenticated";
+GRANT ALL ON TABLE "public"."schedule" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."subject" TO "anon";
+GRANT ALL ON TABLE "public"."subject" TO "authenticated";
+GRANT ALL ON TABLE "public"."subject" TO "service_role";
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+RESET ALL;
