574f1221d47750ac9a376e6fe543240b01b60a3e
Yncy0 <paalayancy@gmail.com>
2025-01-26 19:44:25 +0800

    added edge to edge

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import AuthProvider from "@/providers/AuthProvider";
 import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SystemBars } from "react-native-edge-to-edge";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
-import * as NavigationBar from "expo-navigation-bar";
 import {
   useUpdateScheduleDone,
   useUpdateScheduleOngoing,
@@ -18,10 +17,6 @@ import {
 import { useUpdateBookedRoomStatus } from "@/hooks/queries/bookedRooms/useUpdateBookedRooms";
 
 SplashScreen.preventAutoHideAsync();
-
-NavigationBar.setPositionAsync("absolute");
-NavigationBar.setBackgroundColorAsync("#ffffff00");
-NavigationBar.setButtonStyleAsync("light");
 
 export default function RootLayout() {
   const queryCLient = new QueryClient();
@@ -46,8 +41,8 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <StatusBar style="auto" backgroundColor="transparent" translucent />
       <QueryClientProvider client={queryCLient}>
+        <SystemBars style="auto" />
         <AuthProvider>
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
