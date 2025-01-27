9c250961bb6c587ec7cb45400f2ab16c8a369f20
Yncy0 <paalayancy@gmail.com>
2025-01-27 19:50:42 +0800

    added modal

--- components/buttons/FABbooking.tsx
+++ components/buttons/FABbooking.tsx
@@ -1,20 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, Modal, Pressable } from "react-native";
 import React from "react";
 import { FAB } from "@rneui/themed";
 import { primaryColor } from "@/constants/Colors";
 
 const FABbooking = () => {
+  const [visible, setVisible] = React.useState(false);
+
   return (
-    <FAB
-      icon={{ name: "add", color: "white" }}
-      color={primaryColor}
-      style={{
-        alignSelf: "flex-end",
-        padding: 20,
-        backgroundColor: "transparent",
-        zIndex: 999,
-      }}
-    />
+    <>
+      <FAB
+        icon={{ name: "add", color: "white" }}
+        color={primaryColor}
+        style={{
+          alignSelf: "flex-end",
+          padding: 20,
+          backgroundColor: "transparent",
+          zIndex: 999,
+        }}
+        onPress={() => setVisible(true)}
+      />
+      <Modal visible={visible}>
+        <View style={{ width: 250, height: 250 }}>
+          <Text>MODAL TEST</Text>
+          <Pressable onPress={() => setVisible(false)}>
+            <Text>Cancel</Text>
+          </Pressable>
+        </View>
+      </Modal>
+    </>
   );
 };
 
