0e062052ca8c5dd0035dce20a2b777bd9334fa68
Yncy0 <paalayancy@gmail.com>
2025-01-29 22:01:41 +0800

    finally can pull supabase

--- supabase/functions/notifications/index.ts
+++ supabase/functions/notifications/index.ts
@@ -8,7 +8,8 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 console.log("Hello from Functions!");
 
 Deno.serve(async (req) => {
-  const expoPushToken = "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]";
+  const expoPushToken =
+    "ExponentPushToken[GLSN-S8Gt4A-CZrCita-_dBkKi0iPuZ0ZkbOVV5r]";
 
   const res = await fetch("https://exp.host/--/api/v2/push/send", {
     method: "POST",
@@ -17,7 +18,7 @@ Deno.serve(async (req) => {
       "Authorization": `Bearer ${Deno.env.get(`EXPO_ACCESS_TOKEN`)}`,
     },
     body: JSON.stringify({
-      to: "ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]",
+      to: "ExponentPushToken[GLSN-S8Gt4A-CZrCita-_dBkKi0iPuZ0ZkbOVV5r]",
       sound: "default",
       body: "Hello world!",
     }),
